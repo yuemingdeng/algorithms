5a56b033eb156abb7567a6c81e45f8977b368592
yuemingdeng <391316512@qq.com>
2022-11-24 15:09:13 +0800

    Update BubbleSort.java
    增加判空，修改边界限制写法，修改打印方式；

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -6,20 +6,17 @@ package sort;
  *
  */
 public class BubbleSort {
-
     public static void main(String[] args) {
         int [] a = {5,1,4,6,4,6,10,9,8};
-        bubbleSort(a);
-        for(int ss : a) {
-            System.out.println(ss);
-        }
-
+        System.out.println(Arrays.toString(bubbleSort(a)));
     }
-
-
+    
     public static int[] bubbleSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
+        if(array == null || array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i <= array.length -1; i++) {
+            for (int j = 0; j <= array.length - i - 2; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
